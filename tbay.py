2f1cd1d46e681f77f67c02ca3473935d7bda6e34
Jamie Cannone <jjcannone@bluehen.udel.edu>
2015-02-05 17:10:23 +0000

    initial commit

---
+++ tbay.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://action:action@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Item(Base):
+  __tablename__ = "items"
+  id = Column(Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  description = Column(String)
+  start_time = Column(DateTime, default=datetime.utcnow)
+
+class User(Base):
+  __tablename__ = "users"
+  id = Column(Integer, primary_key=True)
+  username = Column(String, nullable=False)
+  password = Column(String, nullable=False)
+
+class Bid(Base):
+  __tablename__ = "bids"
+  id = Column(Integer, primary_key=True)
+  price = Column(Float, nullable=False)
+
+Base.metadata.create_all(engine)
+
+#  Example create user
+#  beyonce = User()
+#  beyonce.username = "bknowles"
+#  beyonce.password = "crazyinlove"
+#  beyonce = User(username="bknowles", password="crazyinlove") ### short form
+#  session.add(beyonce)
+#  session.commit()
+
+#  Example queries
+#  Returns a list of all of the user objects
+#  session.query(User).all() # Returns a list of all of the user objects
+#
+#  Returns the first user
+#  session.query(User).first()
+#
+#  Finds the user with the primary key equal to 1
+#  session.query(User).get(1)
+#
+#  Returns a list of all of the usernames in ascending order
+#  session.query(User.username).order_by(User.username).all()
+#
+#  Returns the description of all of the basesballs
+#  session.query(Item.description).filter(Item.name == "baseball").all()
+#
+#  Return the item id and description for all baseballs which were created in the past.
+#  Remember to import the datetime object: from datetime import datetime
+#  session.query(Item.id, Item.description).filter(Item.name == "baseball", 
+#                                                  Item.start_time < datetime.utcnow()).all()
+
+#  Updating rows
+#  user = session.query(User).first()
+#  user.username = "solange"
+#  session.commit()
+
+#  Deleting rows
+#  user = session.query(User).first()
+#  session.delete(user)
+#  session.commit()
